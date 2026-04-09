472ab434883feec12f31132f483a5a5c9c7081b9
Konstantin S. <havendv@gmail.com>
2026-04-09 11:12:38 +0400

    feat: Updated OpenAPI spec (#83)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.ImageexplicitContentResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.ImageexplicitContentResponseModel.g.cs
@@ -11,26 +11,14 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Amazon { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("sentisight")]
         public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Sentisight { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Openai { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
-        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Google { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
-        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Microsoft { get; set; }
 
         /// <summary>
         /// 
@@ -47,8 +35,20 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Amazon { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Openai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
+        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Microsoft { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
+        public global::EdenAI.ImageexplicitContentExplicitContentDataClass? Google { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="ImageexplicitContentResponseModel" /> class.
         /// </summary>
+        /// <param name="amazon"></param>
         /// <param name="sentisight"></param>
-        /// <param name="openai"></param>
-        /// <param name="google"></param>
-        /// <param name="microsoft"></param>
         /// <param name="clarifai"></param>
         /// <param name="api4ai"></param>
-        /// <param name="amazon"></param>
+        /// <param name="openai"></param>
+        /// <param name="microsoft"></param>
+        /// <param name="google"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public ImageexplicitContentResponseModel(
+            global::EdenAI.ImageexplicitContentExplicitContentDataClass? amazon,
             global::EdenAI.ImageexplicitContentExplicitContentDataClass? sentisight,
-            global::EdenAI.ImageexplicitContentExplicitContentDataClass? openai,
-            global::EdenAI.ImageexplicitContentExplicitContentDataClass? google,
-            global::EdenAI.ImageexplicitContentExplicitContentDataClass? microsoft,
             global::EdenAI.ImageexplicitContentExplicitContentDataClass? clarifai,
             global::EdenAI.ImageexplicitContentExplicitContentDataClass? api4ai,
-            global::EdenAI.ImageexplicitContentExplicitContentDataClass? amazon)
+            global::EdenAI.ImageexplicitContentExplicitContentDataClass? openai,
+            global::EdenAI.ImageexplicitContentExplicitContentDataClass? microsoft,
+            global::EdenAI.ImageexplicitContentExplicitContentDataClass? google)
         {
+            this.Amazon = amazon;
             this.Sentisight = sentisight;
-            this.Openai = openai;
-            this.Google = google;
-            this.Microsoft = microsoft;
             this.Clarifai = clarifai;
             this.Api4ai = api4ai;
-            this.Amazon = amazon;
+            this.Openai = openai;
+            this.Microsoft = microsoft;
+            this.Google = google;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.OcrocrAsyncModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.OcrocrAsyncModel.g.cs
@@ -11,14 +11,14 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("mistral")]
-        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Mistral { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Amazon { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
-        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Google { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("mistral")]
+        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Mistral { get; set; }
 
         /// <summary>
         /// 
@@ -29,14 +29,14 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Amazon { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
+        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Oneai { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
-        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Oneai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
+        public global::EdenAI.OcrocrAsyncOcrAsyncDataClass? Google { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -47,26 +47,26 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="OcrocrAsyncModel" /> class.
         /// </summary>
-        /// <param name="mistral"></param>
-        /// <param name="google"></param>
-        /// <param name="microsoft"></param>
         /// <param name="amazon"></param>
+        /// <param name="mistral"></param>
+        /// <param name="microsoft"></param>
         /// <param name="oneai"></param>
+        /// <param name="google"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public OcrocrAsyncModel(
-            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? mistral,
-            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? google,
-            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? microsoft,
             global::EdenAI.OcrocrAsyncOcrAsyncDataClass? amazon,
-            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? oneai)
+            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? mistral,
+            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? microsoft,
+            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? oneai,
+            global::EdenAI.OcrocrAsyncOcrAsyncDataClass? google)
         {
-            this.Mistral = mistral;
-            this.Google = google;
-            this.Microsoft = microsoft;
             this.Amazon = amazon;
+            this.Mistral = mistral;
+            this.Microsoft = microsoft;
             this.Oneai = oneai;
+            this.Google = google;
         }
 
         /// <summary>
