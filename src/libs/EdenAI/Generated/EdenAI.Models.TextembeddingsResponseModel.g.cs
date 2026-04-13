6d09341317e4dbc1daa7c4a7df5a5e40a6144aef
Konstantin S. <havendv@gmail.com>
2026-04-13 20:07:58 +0400

    feat: Updated OpenAPI spec (#116)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.TextembeddingsResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextembeddingsResponseModel.g.cs
@@ -17,26 +17,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("jina")]
-        public global::EdenAI.TextembeddingsEmbeddingsDataClass? Jina { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("cohere")]
-        public global::EdenAI.TextembeddingsEmbeddingsDataClass? Cohere { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("mistral")]
         public global::EdenAI.TextembeddingsEmbeddingsDataClass? Mistral { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.TextembeddingsEmbeddingsDataClass? Openai { get; set; }
 
         /// <summary>
         /// 
@@ -47,8 +29,26 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("jina")]
+        public global::EdenAI.TextembeddingsEmbeddingsDataClass? Jina { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.TextembeddingsEmbeddingsDataClass? Openai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("iointelligence")]
         public global::EdenAI.TextembeddingsEmbeddingsDataClass? Iointelligence { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("cohere")]
+        public global::EdenAI.TextembeddingsEmbeddingsDataClass? Cohere { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -60,31 +60,31 @@ namespace EdenAI
         /// Initializes a new instance of the <see cref="TextembeddingsResponseModel" /> class.
         /// </summary>
         /// <param name="ai21labs"></param>
-        /// <param name="jina"></param>
-        /// <param name="cohere"></param>
         /// <param name="mistral"></param>
-        /// <param name="openai"></param>
         /// <param name="google"></param>
+        /// <param name="jina"></param>
+        /// <param name="openai"></param>
         /// <param name="iointelligence"></param>
+        /// <param name="cohere"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextembeddingsResponseModel(
             global::EdenAI.TextembeddingsEmbeddingsDataClass? ai21labs,
-            global::EdenAI.TextembeddingsEmbeddingsDataClass? jina,
-            global::EdenAI.TextembeddingsEmbeddingsDataClass? cohere,
             global::EdenAI.TextembeddingsEmbeddingsDataClass? mistral,
-            global::EdenAI.TextembeddingsEmbeddingsDataClass? openai,
             global::EdenAI.TextembeddingsEmbeddingsDataClass? google,
-            global::EdenAI.TextembeddingsEmbeddingsDataClass? iointelligence)
+            global::EdenAI.TextembeddingsEmbeddingsDataClass? jina,
+            global::EdenAI.TextembeddingsEmbeddingsDataClass? openai,
+            global::EdenAI.TextembeddingsEmbeddingsDataClass? iointelligence,
+            global::EdenAI.TextembeddingsEmbeddingsDataClass? cohere)
         {
             this.Ai21labs = ai21labs;
-            this.Jina = jina;
-            this.Cohere = cohere;
             this.Mistral = mistral;
-            this.Openai = openai;
             this.Google = google;
+            this.Jina = jina;
+            this.Openai = openai;
             this.Iointelligence = iointelligence;
+            this.Cohere = cohere;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.TextspellCheckResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextspellCheckResponseModel.g.cs
@@ -11,6 +11,24 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("xai")]
+        public global::EdenAI.TextspellCheckSpellCheckDataClass? Xai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("sapling")]
+        public global::EdenAI.TextspellCheckSpellCheckDataClass? Sapling { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.TextspellCheckSpellCheckDataClass? Openai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
         public global::EdenAI.TextspellCheckSpellCheckDataClass? Microsoft { get; set; }
 
@@ -23,26 +41,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("xai")]
-        public global::EdenAI.TextspellCheckSpellCheckDataClass? Xai { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("prowritingaid")]
         public global::EdenAI.TextspellCheckSpellCheckDataClass? Prowritingaid { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.TextspellCheckSpellCheckDataClass? Openai { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sapling")]
-        public global::EdenAI.TextspellCheckSpellCheckDataClass? Sapling { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -53,29 +53,29 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TextspellCheckResponseModel" /> class.
         /// </summary>
+        /// <param name="xai"></param>
+        /// <param name="sapling"></param>
+        /// <param name="openai"></param>
         /// <param name="microsoft"></param>
         /// <param name="cohere"></param>
-        /// <param name="xai"></param>
         /// <param name="prowritingaid"></param>
-        /// <param name="openai"></param>
-        /// <param name="sapling"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextspellCheckResponseModel(
+            global::EdenAI.TextspellCheckSpellCheckDataClass? xai,
+            global::EdenAI.TextspellCheckSpellCheckDataClass? sapling,
+            global::EdenAI.TextspellCheckSpellCheckDataClass? openai,
             global::EdenAI.TextspellCheckSpellCheckDataClass? microsoft,
             global::EdenAI.TextspellCheckSpellCheckDataClass? cohere,
-            global::EdenAI.TextspellCheckSpellCheckDataClass? xai,
-            global::EdenAI.TextspellCheckSpellCheckDataClass? prowritingaid,
-            global::EdenAI.TextspellCheckSpellCheckDataClass? openai,
-            global::EdenAI.TextspellCheckSpellCheckDataClass? sapling)
+            global::EdenAI.TextspellCheckSpellCheckDataClass? prowritingaid)
         {
+            this.Xai = xai;
+            this.Sapling = sapling;
+            this.Openai = openai;
             this.Microsoft = microsoft;
             this.Cohere = cohere;
-            this.Xai = xai;
             this.Prowritingaid = prowritingaid;
-            this.Openai = openai;
-            this.Sapling = sapling;
         }
 
         /// <summary>
