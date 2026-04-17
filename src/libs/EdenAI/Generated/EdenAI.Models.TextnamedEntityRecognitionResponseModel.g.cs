d9467cbe12b2028c9955a35c76c6846d4d1509ed
Konstantin S. <havendv@gmail.com>
2026-04-17 19:52:34 +0400

    feat: Updated OpenAPI spec (#145)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.TextnamedEntityRecognitionResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextnamedEntityRecognitionResponseModel.g.cs
@@ -11,32 +11,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Amazon { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
-        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Microsoft { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("tenstorrent")]
         public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Tenstorrent { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("xai")]
-        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Xai { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Openai { get; set; }
 
         /// <summary>
         /// 
@@ -51,6 +27,30 @@ namespace EdenAI
         public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Oneai { get; set; }
 
         /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Openai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
+        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Microsoft { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("xai")]
+        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Xai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? Amazon { get; set; }
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TextnamedEntityRecognitionResponseModel" /> class.
         /// </summary>
-        /// <param name="amazon"></param>
-        /// <param name="microsoft"></param>
         /// <param name="tenstorrent"></param>
-        /// <param name="xai"></param>
-        /// <param name="openai"></param>
         /// <param name="google"></param>
         /// <param name="oneai"></param>
+        /// <param name="openai"></param>
+        /// <param name="microsoft"></param>
+        /// <param name="xai"></param>
+        /// <param name="amazon"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextnamedEntityRecognitionResponseModel(
-            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? amazon,
-            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? microsoft,
             global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? tenstorrent,
-            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? xai,
-            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? openai,
             global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? google,
-            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? oneai)
+            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? oneai,
+            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? openai,
+            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? microsoft,
+            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? xai,
+            global::EdenAI.TextnamedEntityRecognitionNamedEntityRecognitionDataClass? amazon)
         {
-            this.Amazon = amazon;
-            this.Microsoft = microsoft;
             this.Tenstorrent = tenstorrent;
-            this.Xai = xai;
-            this.Openai = openai;
             this.Google = google;
             this.Oneai = oneai;
+            this.Openai = openai;
+            this.Microsoft = microsoft;
+            this.Xai = xai;
+            this.Amazon = amazon;
         }
 
         /// <summary>
