2f775bd7ac2947a7d5f74808c8731c2dfd4eee54
Konstantin S. <havendv@gmail.com>
2026-04-28 09:51:21 +0400

    feat: Updated OpenAPI spec (#228)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.AudiotextToSpeechResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.AudiotextToSpeechResponseModel.g.cs
@@ -11,24 +11,6 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Microsoft { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("elevenlabs")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Elevenlabs { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("deepgram")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Deepgram { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
         public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Amazon { get; set; }
 
@@ -41,14 +23,32 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("lovoai")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Lovoai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("deepgram")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Deepgram { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("google")]
         public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Google { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("lovoai")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Lovoai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("elevenlabs")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Elevenlabs { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Microsoft { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="AudiotextToSpeechResponseModel" /> class.
         /// </summary>
-        /// <param name="microsoft"></param>
-        /// <param name="elevenlabs"></param>
-        /// <param name="deepgram"></param>
         /// <param name="amazon"></param>
         /// <param name="openai"></param>
-        /// <param name="google"></param>
         /// <param name="lovoai"></param>
+        /// <param name="deepgram"></param>
+        /// <param name="google"></param>
+        /// <param name="elevenlabs"></param>
+        /// <param name="microsoft"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public AudiotextToSpeechResponseModel(
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? microsoft,
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? elevenlabs,
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? deepgram,
             global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? amazon,
             global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? openai,
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? lovoai,
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? deepgram,
             global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? google,
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? lovoai)
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? elevenlabs,
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? microsoft)
         {
-            this.Microsoft = microsoft;
-            this.Elevenlabs = elevenlabs;
-            this.Deepgram = deepgram;
             this.Amazon = amazon;
             this.Openai = openai;
-            this.Google = google;
             this.Lovoai = lovoai;
+            this.Deepgram = deepgram;
+            this.Google = google;
+            this.Elevenlabs = elevenlabs;
+            this.Microsoft = microsoft;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.ImagebackgroundRemovalResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.ImagebackgroundRemovalResponseModel.g.cs
@@ -11,14 +11,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("stabilityai")]
-        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Stabilityai { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sentisight")]
-        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Sentisight { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("picsart")]
+        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Picsart { get; set; }
 
         /// <summary>
         /// 
@@ -29,8 +23,14 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
-        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Microsoft { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("api4ai")]
+        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Api4ai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("sentisight")]
+        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Sentisight { get; set; }
 
         /// <summary>
         /// 
@@ -41,14 +41,14 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("picsart")]
-        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Picsart { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("stabilityai")]
+        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Stabilityai { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("api4ai")]
-        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Api4ai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
+        public global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? Microsoft { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="ImagebackgroundRemovalResponseModel" /> class.
         /// </summary>
-        /// <param name="stabilityai"></param>
-        /// <param name="sentisight"></param>
-        /// <param name="photoroom"></param>
-        /// <param name="microsoft"></param>
-        /// <param name="clipdrop"></param>
         /// <param name="picsart"></param>
+        /// <param name="photoroom"></param>
         /// <param name="api4ai"></param>
+        /// <param name="sentisight"></param>
+        /// <param name="clipdrop"></param>
+        /// <param name="stabilityai"></param>
+        /// <param name="microsoft"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public ImagebackgroundRemovalResponseModel(
-            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? stabilityai,
-            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? sentisight,
-            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? photoroom,
-            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? microsoft,
-            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? clipdrop,
             global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? picsart,
-            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? api4ai)
+            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? photoroom,
+            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? api4ai,
+            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? sentisight,
+            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? clipdrop,
+            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? stabilityai,
+            global::EdenAI.ImagebackgroundRemovalBackgroundRemovalDataClass? microsoft)
         {
-            this.Stabilityai = stabilityai;
-            this.Sentisight = sentisight;
-            this.Photoroom = photoroom;
-            this.Microsoft = microsoft;
-            this.Clipdrop = clipdrop;
             this.Picsart = picsart;
+            this.Photoroom = photoroom;
             this.Api4ai = api4ai;
+            this.Sentisight = sentisight;
+            this.Clipdrop = clipdrop;
+            this.Stabilityai = stabilityai;
+            this.Microsoft = microsoft;
         }
 
         /// <summary>
