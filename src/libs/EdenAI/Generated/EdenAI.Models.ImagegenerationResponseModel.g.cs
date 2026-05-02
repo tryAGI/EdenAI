a164311312591bd0a1b108769c20034df1edb07c
Konstantin S. <havendv@gmail.com>
2026-05-03 01:33:02 +0400

    feat: Updated OpenAPI spec (#258)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.ImagegenerationResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.ImagegenerationResponseModel.g.cs
@@ -17,14 +17,20 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("bytedance")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Bytedance { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("stabilityai")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Stabilityai { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("stabilityai")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Stabilityai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Amazon { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("minimax")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Minimax { get; set; }
 
         /// <summary>
         /// 
@@ -47,14 +53,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Amazon { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("minimax")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Minimax { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("bytedance")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Bytedance { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -66,34 +66,34 @@ namespace EdenAI
         /// Initializes a new instance of the <see cref="ImagegenerationResponseModel" /> class.
         /// </summary>
         /// <param name="replicate"></param>
-        /// <param name="bytedance"></param>
         /// <param name="stabilityai"></param>
+        /// <param name="amazon"></param>
+        /// <param name="minimax"></param>
         /// <param name="openai"></param>
         /// <param name="deepai"></param>
         /// <param name="leonardo"></param>
-        /// <param name="amazon"></param>
-        /// <param name="minimax"></param>
+        /// <param name="bytedance"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public ImagegenerationResponseModel(
             global::EdenAI.ImagegenerationGenerationDataClass? replicate,
-            global::EdenAI.ImagegenerationGenerationDataClass? bytedance,
             global::EdenAI.ImagegenerationGenerationDataClass? stabilityai,
+            global::EdenAI.ImagegenerationGenerationDataClass? amazon,
+            global::EdenAI.ImagegenerationGenerationDataClass? minimax,
             global::EdenAI.ImagegenerationGenerationDataClass? openai,
             global::EdenAI.ImagegenerationGenerationDataClass? deepai,
             global::EdenAI.ImagegenerationGenerationDataClass? leonardo,
-            global::EdenAI.ImagegenerationGenerationDataClass? amazon,
-            global::EdenAI.ImagegenerationGenerationDataClass? minimax)
+            global::EdenAI.ImagegenerationGenerationDataClass? bytedance)
         {
             this.Replicate = replicate;
-            this.Bytedance = bytedance;
             this.Stabilityai = stabilityai;
+            this.Amazon = amazon;
+            this.Minimax = minimax;
             this.Openai = openai;
             this.Deepai = deepai;
             this.Leonardo = leonardo;
-            this.Amazon = amazon;
-            this.Minimax = minimax;
+            this.Bytedance = bytedance;
         }
 
         /// <summary>
