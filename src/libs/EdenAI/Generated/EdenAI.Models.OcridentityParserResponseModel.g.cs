8249121e42f49bfcb82d5f6f8746fcae6f11f9e3
Konstantin S. <havendv@gmail.com>
2026-05-08 23:05:43 +0400

    feat: Updated OpenAPI spec (#304)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.OcridentityParserResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.OcridentityParserResponseModel.g.cs
@@ -11,14 +11,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
-        public global::EdenAI.OcridentityParserIdentityParserDataClass? Microsoft { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("klippa")]
-        public global::EdenAI.OcridentityParserIdentityParserDataClass? Klippa { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("mindee")]
+        public global::EdenAI.OcridentityParserIdentityParserDataClass? Mindee { get; set; }
 
         /// <summary>
         /// 
@@ -29,14 +23,14 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("mindee")]
-        public global::EdenAI.OcridentityParserIdentityParserDataClass? Mindee { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.OcridentityParserIdentityParserDataClass? Amazon { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.OcridentityParserIdentityParserDataClass? Amazon { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("klippa")]
+        public global::EdenAI.OcridentityParserIdentityParserDataClass? Klippa { get; set; }
 
         /// <summary>
         /// 
@@ -51,6 +45,12 @@ namespace EdenAI
         public global::EdenAI.OcridentityParserIdentityParserDataClass? Base64 { get; set; }
 
         /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
+        public global::EdenAI.OcridentityParserIdentityParserDataClass? Microsoft { get; set; }
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="OcridentityParserResponseModel" /> class.
         /// </summary>
-        /// <param name="microsoft"></param>
-        /// <param name="klippa"></param>
-        /// <param name="openai"></param>
         /// <param name="mindee"></param>
+        /// <param name="openai"></param>
         /// <param name="amazon"></param>
+        /// <param name="klippa"></param>
         /// <param name="affinda"></param>
         /// <param name="base64"></param>
+        /// <param name="microsoft"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public OcridentityParserResponseModel(
-            global::EdenAI.OcridentityParserIdentityParserDataClass? microsoft,
-            global::EdenAI.OcridentityParserIdentityParserDataClass? klippa,
-            global::EdenAI.OcridentityParserIdentityParserDataClass? openai,
             global::EdenAI.OcridentityParserIdentityParserDataClass? mindee,
+            global::EdenAI.OcridentityParserIdentityParserDataClass? openai,
             global::EdenAI.OcridentityParserIdentityParserDataClass? amazon,
+            global::EdenAI.OcridentityParserIdentityParserDataClass? klippa,
             global::EdenAI.OcridentityParserIdentityParserDataClass? affinda,
-            global::EdenAI.OcridentityParserIdentityParserDataClass? base64)
+            global::EdenAI.OcridentityParserIdentityParserDataClass? base64,
+            global::EdenAI.OcridentityParserIdentityParserDataClass? microsoft)
         {
-            this.Microsoft = microsoft;
-            this.Klippa = klippa;
-            this.Openai = openai;
             this.Mindee = mindee;
+            this.Openai = openai;
             this.Amazon = amazon;
+            this.Klippa = klippa;
             this.Affinda = affinda;
             this.Base64 = base64;
+            this.Microsoft = microsoft;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.TexttopicExtractionResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TexttopicExtractionResponseModel.g.cs
@@ -11,8 +11,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
-        public global::EdenAI.TexttopicExtractionTopicExtractionDataClass? Google { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.TexttopicExtractionTopicExtractionDataClass? Openai { get; set; }
 
         /// <summary>
         /// 
@@ -29,8 +29,8 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.TexttopicExtractionTopicExtractionDataClass? Openai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
+        public global::EdenAI.TexttopicExtractionTopicExtractionDataClass? Google { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -41,23 +41,23 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TexttopicExtractionResponseModel" /> class.
         /// </summary>
-        /// <param name="google"></param>
+        /// <param name="openai"></param>
         /// <param name="xai"></param>
         /// <param name="tenstorrent"></param>
-        /// <param name="openai"></param>
+        /// <param name="google"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TexttopicExtractionResponseModel(
-            global::EdenAI.TexttopicExtractionTopicExtractionDataClass? google,
+            global::EdenAI.TexttopicExtractionTopicExtractionDataClass? openai,
             global::EdenAI.TexttopicExtractionTopicExtractionDataClass? xai,
             global::EdenAI.TexttopicExtractionTopicExtractionDataClass? tenstorrent,
-            global::EdenAI.TexttopicExtractionTopicExtractionDataClass? openai)
+            global::EdenAI.TexttopicExtractionTopicExtractionDataClass? google)
         {
-            this.Google = google;
+            this.Openai = openai;
             this.Xai = xai;
             this.Tenstorrent = tenstorrent;
-            this.Openai = openai;
+            this.Google = google;
         }
 
         /// <summary>
