8e8817ba26f1f411c3aa2de05131d0ce32003cb4
Konstantin S. <havendv@gmail.com>
2026-05-19 18:39:36 +0400

    feat: Updated OpenAPI spec (#370)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.OcrbankCheckParsingResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.OcrbankCheckParsingResponseModel.g.cs
@@ -11,6 +11,12 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("veryfi")]
+        public global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? Veryfi { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("base64")]
         public global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? Base64 { get; set; }
 
@@ -19,12 +25,6 @@ namespace EdenAI
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("extracta")]
         public global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? Extracta { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("veryfi")]
-        public global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? Veryfi { get; set; }
 
         /// <summary>
         /// 
@@ -41,22 +41,22 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="OcrbankCheckParsingResponseModel" /> class.
         /// </summary>
+        /// <param name="veryfi"></param>
         /// <param name="base64"></param>
         /// <param name="extracta"></param>
-        /// <param name="veryfi"></param>
         /// <param name="mindee"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public OcrbankCheckParsingResponseModel(
+            global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? veryfi,
             global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? base64,
             global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? extracta,
-            global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? veryfi,
             global::EdenAI.OcrbankCheckParsingBankCheckParsingDataClass? mindee)
         {
+            this.Veryfi = veryfi;
             this.Base64 = base64;
             this.Extracta = extracta;
-            this.Veryfi = veryfi;
             this.Mindee = mindee;
         }
 
--- src/libs/EdenAI/Generated/EdenAI.Models.TextaiDetectionResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextaiDetectionResponseModel.g.cs
@@ -17,14 +17,14 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("originalityai")]
-        public global::EdenAI.TextaiDetectionAiDetectionDataClass? Originalityai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("winstonai")]
+        public global::EdenAI.TextaiDetectionAiDetectionDataClass? Winstonai { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("winstonai")]
-        public global::EdenAI.TextaiDetectionAiDetectionDataClass? Winstonai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("originalityai")]
+        public global::EdenAI.TextaiDetectionAiDetectionDataClass? Originalityai { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -36,19 +36,19 @@ namespace EdenAI
         /// Initializes a new instance of the <see cref="TextaiDetectionResponseModel" /> class.
         /// </summary>
         /// <param name="sapling"></param>
-        /// <param name="originalityai"></param>
         /// <param name="winstonai"></param>
+        /// <param name="originalityai"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextaiDetectionResponseModel(
             global::EdenAI.TextaiDetectionAiDetectionDataClass? sapling,
-            global::EdenAI.TextaiDetectionAiDetectionDataClass? originalityai,
-            global::EdenAI.TextaiDetectionAiDetectionDataClass? winstonai)
+            global::EdenAI.TextaiDetectionAiDetectionDataClass? winstonai,
+            global::EdenAI.TextaiDetectionAiDetectionDataClass? originalityai)
         {
             this.Sapling = sapling;
-            this.Originalityai = originalityai;
             this.Winstonai = winstonai;
+            this.Originalityai = originalityai;
         }
 
         /// <summary>
