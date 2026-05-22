bb77b8808b49ff2bb6eae6d63f3048cf7119a123
Konstantin S. <havendv@gmail.com>
2026-05-22 06:33:31 +0400

    feat: Updated OpenAPI spec (#386)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.MultimodalchatResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.MultimodalchatResponseModel.g.cs
@@ -11,24 +11,6 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
-        public global::EdenAI.MultimodalchatChatDataClass? Google { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("mistral")]
-        public global::EdenAI.MultimodalchatChatDataClass? Mistral { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.MultimodalchatChatDataClass? Amazon { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
         public global::EdenAI.MultimodalchatChatDataClass? Microsoft { get; set; }
 
@@ -51,6 +33,24 @@ namespace EdenAI
         public global::EdenAI.MultimodalchatChatDataClass? Openai { get; set; }
 
         /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("mistral")]
+        public global::EdenAI.MultimodalchatChatDataClass? Mistral { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.MultimodalchatChatDataClass? Amazon { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
+        public global::EdenAI.MultimodalchatChatDataClass? Google { get; set; }
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="MultimodalchatResponseModel" /> class.
         /// </summary>
-        /// <param name="google"></param>
-        /// <param name="mistral"></param>
-        /// <param name="amazon"></param>
         /// <param name="microsoft"></param>
         /// <param name="xai"></param>
         /// <param name="anthropic"></param>
         /// <param name="openai"></param>
+        /// <param name="mistral"></param>
+        /// <param name="amazon"></param>
+        /// <param name="google"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public MultimodalchatResponseModel(
-            global::EdenAI.MultimodalchatChatDataClass? google,
-            global::EdenAI.MultimodalchatChatDataClass? mistral,
-            global::EdenAI.MultimodalchatChatDataClass? amazon,
             global::EdenAI.MultimodalchatChatDataClass? microsoft,
             global::EdenAI.MultimodalchatChatDataClass? xai,
             global::EdenAI.MultimodalchatChatDataClass? anthropic,
-            global::EdenAI.MultimodalchatChatDataClass? openai)
+            global::EdenAI.MultimodalchatChatDataClass? openai,
+            global::EdenAI.MultimodalchatChatDataClass? mistral,
+            global::EdenAI.MultimodalchatChatDataClass? amazon,
+            global::EdenAI.MultimodalchatChatDataClass? google)
         {
-            this.Google = google;
-            this.Mistral = mistral;
-            this.Amazon = amazon;
             this.Microsoft = microsoft;
             this.Xai = xai;
             this.Anthropic = anthropic;
             this.Openai = openai;
+            this.Mistral = mistral;
+            this.Amazon = amazon;
+            this.Google = google;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.TextanonymizationResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextanonymizationResponseModel.g.cs
@@ -11,18 +11,6 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Emvista { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Amazon { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
         public global::EdenAI.TextanonymizationAnonymizationDataClass? Microsoft { get; set; }
 
@@ -47,8 +35,20 @@ namespace EdenAI
         /// <summary>
         /// 
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Amazon { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
         public global::EdenAI.TextanonymizationAnonymizationDataClass? Oneai { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Emvista { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TextanonymizationResponseModel" /> class.
         /// </summary>
-        /// <param name="emvista"></param>
-        /// <param name="amazon"></param>
         /// <param name="microsoft"></param>
         /// <param name="xai"></param>
         /// <param name="openai"></param>
         /// <param name="privateai"></param>
+        /// <param name="amazon"></param>
         /// <param name="oneai"></param>
+        /// <param name="emvista"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextanonymizationResponseModel(
-            global::EdenAI.TextanonymizationAnonymizationDataClass? emvista,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? amazon,
             global::EdenAI.TextanonymizationAnonymizationDataClass? microsoft,
             global::EdenAI.TextanonymizationAnonymizationDataClass? xai,
             global::EdenAI.TextanonymizationAnonymizationDataClass? openai,
             global::EdenAI.TextanonymizationAnonymizationDataClass? privateai,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? oneai)
+            global::EdenAI.TextanonymizationAnonymizationDataClass? amazon,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? oneai,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? emvista)
         {
-            this.Emvista = emvista;
-            this.Amazon = amazon;
             this.Microsoft = microsoft;
             this.Xai = xai;
             this.Openai = openai;
             this.Privateai = privateai;
+            this.Amazon = amazon;
             this.Oneai = oneai;
+            this.Emvista = emvista;
         }
 
         /// <summary>
