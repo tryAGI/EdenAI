4e980490f00d1eaa963008323fd1c7a1af800e91
Konstantin S. <havendv@gmail.com>
2026-09-03 22:16:43 +0400

    feat: Updated OpenAPI spec (#428)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.ImagegenerationResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.ImagegenerationResponseModel.g.cs
@@ -17,14 +17,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Google { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("bytedance")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Bytedance { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("stabilityai")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Stabilityai { get; set; }
 
         /// <summary>
         ///
@@ -41,8 +35,14 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("leonardo")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Leonardo { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("replicate")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Replicate { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("bytedance")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Bytedance { get; set; }
 
         /// <summary>
         ///
@@ -53,14 +53,14 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("stabilityai")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Stabilityai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Google { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("replicate")]
-        public global::EdenAI.ImagegenerationGenerationDataClass? Replicate { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("leonardo")]
+        public global::EdenAI.ImagegenerationGenerationDataClass? Leonardo { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -72,37 +72,37 @@ namespace EdenAI
         /// Initializes a new instance of the <see cref="ImagegenerationResponseModel" /> class.
         /// </summary>
         /// <param name="amazon"></param>
-        /// <param name="google"></param>
-        /// <param name="bytedance"></param>
+        /// <param name="stabilityai"></param>
         /// <param name="minimax"></param>
         /// <param name="deepai"></param>
-        /// <param name="leonardo"></param>
-        /// <param name="openai"></param>
-        /// <param name="stabilityai"></param>
         /// <param name="replicate"></param>
+        /// <param name="bytedance"></param>
+        /// <param name="openai"></param>
+        /// <param name="google"></param>
+        /// <param name="leonardo"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public ImagegenerationResponseModel(
             global::EdenAI.ImagegenerationGenerationDataClass? amazon,
-            global::EdenAI.ImagegenerationGenerationDataClass? google,
-            global::EdenAI.ImagegenerationGenerationDataClass? bytedance,
+            global::EdenAI.ImagegenerationGenerationDataClass? stabilityai,
             global::EdenAI.ImagegenerationGenerationDataClass? minimax,
             global::EdenAI.ImagegenerationGenerationDataClass? deepai,
-            global::EdenAI.ImagegenerationGenerationDataClass? leonardo,
+            global::EdenAI.ImagegenerationGenerationDataClass? replicate,
+            global::EdenAI.ImagegenerationGenerationDataClass? bytedance,
             global::EdenAI.ImagegenerationGenerationDataClass? openai,
-            global::EdenAI.ImagegenerationGenerationDataClass? stabilityai,
-            global::EdenAI.ImagegenerationGenerationDataClass? replicate)
+            global::EdenAI.ImagegenerationGenerationDataClass? google,
+            global::EdenAI.ImagegenerationGenerationDataClass? leonardo)
         {
             this.Amazon = amazon;
-            this.Google = google;
-            this.Bytedance = bytedance;
+            this.Stabilityai = stabilityai;
             this.Minimax = minimax;
             this.Deepai = deepai;
-            this.Leonardo = leonardo;
-            this.Openai = openai;
-            this.Stabilityai = stabilityai;
             this.Replicate = replicate;
+            this.Bytedance = bytedance;
+            this.Openai = openai;
+            this.Google = google;
+            this.Leonardo = leonardo;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.TextanonymizationResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextanonymizationResponseModel.g.cs
@@ -11,20 +11,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Amazon { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
         public global::EdenAI.TextanonymizationAnonymizationDataClass? Microsoft { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Oneai { get; set; }
 
         /// <summary>
         ///
@@ -35,8 +23,20 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Amazon { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
         public global::EdenAI.TextanonymizationAnonymizationDataClass? Openai { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Emvista { get; set; }
 
         /// <summary>
         ///
@@ -47,8 +47,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Emvista { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Oneai { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TextanonymizationResponseModel" /> class.
         /// </summary>
-        /// <param name="amazon"></param>
         /// <param name="microsoft"></param>
-        /// <param name="oneai"></param>
         /// <param name="privateai"></param>
+        /// <param name="amazon"></param>
         /// <param name="openai"></param>
-        /// <param name="xai"></param>
         /// <param name="emvista"></param>
+        /// <param name="xai"></param>
+        /// <param name="oneai"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextanonymizationResponseModel(
-            global::EdenAI.TextanonymizationAnonymizationDataClass? amazon,
             global::EdenAI.TextanonymizationAnonymizationDataClass? microsoft,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? oneai,
             global::EdenAI.TextanonymizationAnonymizationDataClass? privateai,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? amazon,
             global::EdenAI.TextanonymizationAnonymizationDataClass? openai,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? emvista,
             global::EdenAI.TextanonymizationAnonymizationDataClass? xai,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? emvista)
+            global::EdenAI.TextanonymizationAnonymizationDataClass? oneai)
         {
-            this.Amazon = amazon;
             this.Microsoft = microsoft;
-            this.Oneai = oneai;
             this.Privateai = privateai;
+            this.Amazon = amazon;
             this.Openai = openai;
-            this.Xai = xai;
             this.Emvista = emvista;
+            this.Xai = xai;
+            this.Oneai = oneai;
         }
 
         /// <summary>
