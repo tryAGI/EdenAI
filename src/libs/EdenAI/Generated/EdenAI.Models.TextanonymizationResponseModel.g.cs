4d118841988ed2fe7a77676b70ebdacd97b85527
Konstantin S. <havendv@gmail.com>
2026-09-03 17:09:36 +0400

    feat: Updated OpenAPI spec (#427)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.TextanonymizationResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextanonymizationResponseModel.g.cs
@@ -11,8 +11,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Openai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Amazon { get; set; }
 
         /// <summary>
         ///
@@ -23,14 +23,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("xai")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Xai { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Amazon { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Oneai { get; set; }
 
         /// <summary>
         ///
@@ -41,14 +35,20 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Emvista { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Openai { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
-        public global::EdenAI.TextanonymizationAnonymizationDataClass? Oneai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("xai")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Xai { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
+        public global::EdenAI.TextanonymizationAnonymizationDataClass? Emvista { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TextanonymizationResponseModel" /> class.
         /// </summary>
-        /// <param name="openai"></param>
-        /// <param name="microsoft"></param>
-        /// <param name="xai"></param>
         /// <param name="amazon"></param>
-        /// <param name="privateai"></param>
-        /// <param name="emvista"></param>
+        /// <param name="microsoft"></param>
         /// <param name="oneai"></param>
+        /// <param name="privateai"></param>
+        /// <param name="openai"></param>
+        /// <param name="xai"></param>
+        /// <param name="emvista"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextanonymizationResponseModel(
-            global::EdenAI.TextanonymizationAnonymizationDataClass? openai,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? microsoft,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? xai,
             global::EdenAI.TextanonymizationAnonymizationDataClass? amazon,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? microsoft,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? oneai,
             global::EdenAI.TextanonymizationAnonymizationDataClass? privateai,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? emvista,
-            global::EdenAI.TextanonymizationAnonymizationDataClass? oneai)
+            global::EdenAI.TextanonymizationAnonymizationDataClass? openai,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? xai,
+            global::EdenAI.TextanonymizationAnonymizationDataClass? emvista)
         {
-            this.Openai = openai;
-            this.Microsoft = microsoft;
-            this.Xai = xai;
             this.Amazon = amazon;
-            this.Privateai = privateai;
-            this.Emvista = emvista;
+            this.Microsoft = microsoft;
             this.Oneai = oneai;
+            this.Privateai = privateai;
+            this.Openai = openai;
+            this.Xai = xai;
+            this.Emvista = emvista;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.TextsummarizeResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextsummarizeResponseModel.g.cs
@@ -11,14 +11,14 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("meaningcloud")]
-        public global::EdenAI.TextsummarizeSummarizeDataClass? Meaningcloud { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("anthropic")]
+        public global::EdenAI.TextsummarizeSummarizeDataClass? Anthropic { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("writesonic")]
-        public global::EdenAI.TextsummarizeSummarizeDataClass? Writesonic { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
+        public global::EdenAI.TextsummarizeSummarizeDataClass? Microsoft { get; set; }
 
         /// <summary>
         ///
@@ -29,8 +29,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("anthropic")]
-        public global::EdenAI.TextsummarizeSummarizeDataClass? Anthropic { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("meaningcloud")]
+        public global::EdenAI.TextsummarizeSummarizeDataClass? Meaningcloud { get; set; }
 
         /// <summary>
         ///
@@ -41,8 +41,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
-        public global::EdenAI.TextsummarizeSummarizeDataClass? Microsoft { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
+        public global::EdenAI.TextsummarizeSummarizeDataClass? Oneai { get; set; }
 
         /// <summary>
         ///
@@ -65,8 +65,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
-        public global::EdenAI.TextsummarizeSummarizeDataClass? Oneai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("writesonic")]
+        public global::EdenAI.TextsummarizeSummarizeDataClass? Writesonic { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -77,41 +77,41 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TextsummarizeResponseModel" /> class.
         /// </summary>
-        /// <param name="meaningcloud"></param>
-        /// <param name="writesonic"></param>
-        /// <param name="alephalpha"></param>
         /// <param name="anthropic"></param>
-        /// <param name="cohere"></param>
         /// <param name="microsoft"></param>
+        /// <param name="alephalpha"></param>
+        /// <param name="meaningcloud"></param>
+        /// <param name="cohere"></param>
+        /// <param name="oneai"></param>
         /// <param name="openai"></param>
         /// <param name="xai"></param>
         /// <param name="emvista"></param>
-        /// <param name="oneai"></param>
+        /// <param name="writesonic"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextsummarizeResponseModel(
-            global::EdenAI.TextsummarizeSummarizeDataClass? meaningcloud,
-            global::EdenAI.TextsummarizeSummarizeDataClass? writesonic,
-            global::EdenAI.TextsummarizeSummarizeDataClass? alephalpha,
             global::EdenAI.TextsummarizeSummarizeDataClass? anthropic,
-            global::EdenAI.TextsummarizeSummarizeDataClass? cohere,
             global::EdenAI.TextsummarizeSummarizeDataClass? microsoft,
+            global::EdenAI.TextsummarizeSummarizeDataClass? alephalpha,
+            global::EdenAI.TextsummarizeSummarizeDataClass? meaningcloud,
+            global::EdenAI.TextsummarizeSummarizeDataClass? cohere,
+            global::EdenAI.TextsummarizeSummarizeDataClass? oneai,
             global::EdenAI.TextsummarizeSummarizeDataClass? openai,
             global::EdenAI.TextsummarizeSummarizeDataClass? xai,
             global::EdenAI.TextsummarizeSummarizeDataClass? emvista,
-            global::EdenAI.TextsummarizeSummarizeDataClass? oneai)
+            global::EdenAI.TextsummarizeSummarizeDataClass? writesonic)
         {
-            this.Meaningcloud = meaningcloud;
-            this.Writesonic = writesonic;
-            this.Alephalpha = alephalpha;
             this.Anthropic = anthropic;
-            this.Cohere = cohere;
             this.Microsoft = microsoft;
+            this.Alephalpha = alephalpha;
+            this.Meaningcloud = meaningcloud;
+            this.Cohere = cohere;
+            this.Oneai = oneai;
             this.Openai = openai;
             this.Xai = xai;
             this.Emvista = emvista;
-            this.Oneai = oneai;
+            this.Writesonic = writesonic;
         }
 
         /// <summary>
