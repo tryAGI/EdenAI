e6467df3b17a06c19602a70b505178974a5bae4b
Konstantin S. <havendv@gmail.com>
2026-09-06 06:03:30 +0400

    feat: Updated OpenAPI spec (#439)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.AudiotextToSpeechResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.AudiotextToSpeechResponseModel.g.cs
@@ -11,20 +11,14 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Openai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("lovoai")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Lovoai { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("elevenlabs")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Elevenlabs { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("deepgram")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Deepgram { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Google { get; set; }
 
         /// <summary>
         ///
@@ -35,20 +29,26 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("lovoai")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Lovoai { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
         public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Microsoft { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
-        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Google { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("deepgram")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Deepgram { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("elevenlabs")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Elevenlabs { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? Openai { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -59,32 +59,32 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="AudiotextToSpeechResponseModel" /> class.
         /// </summary>
-        /// <param name="openai"></param>
-        /// <param name="elevenlabs"></param>
-        /// <param name="deepgram"></param>
-        /// <param name="amazon"></param>
         /// <param name="lovoai"></param>
-        /// <param name="microsoft"></param>
         /// <param name="google"></param>
+        /// <param name="amazon"></param>
+        /// <param name="microsoft"></param>
+        /// <param name="deepgram"></param>
+        /// <param name="elevenlabs"></param>
+        /// <param name="openai"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public AudiotextToSpeechResponseModel(
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? openai,
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? elevenlabs,
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? deepgram,
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? amazon,
             global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? lovoai,
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? google,
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? amazon,
             global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? microsoft,
-            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? google)
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? deepgram,
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? elevenlabs,
+            global::EdenAI.AudiotextToSpeechTextToSpeechDataClass? openai)
         {
-            this.Openai = openai;
-            this.Elevenlabs = elevenlabs;
-            this.Deepgram = deepgram;
-            this.Amazon = amazon;
             this.Lovoai = lovoai;
-            this.Microsoft = microsoft;
             this.Google = google;
+            this.Amazon = amazon;
+            this.Microsoft = microsoft;
+            this.Deepgram = deepgram;
+            this.Elevenlabs = elevenlabs;
+            this.Openai = openai;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.VideodeepfakeDetectionAsyncModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.VideodeepfakeDetectionAsyncModel.g.cs
@@ -11,14 +11,14 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("resemble")]
-        public global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? Resemble { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("sightengine")]
+        public global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? Sightengine { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("sightengine")]
-        public global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? Sightengine { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("resemble")]
+        public global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? Resemble { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -29,17 +29,17 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="VideodeepfakeDetectionAsyncModel" /> class.
         /// </summary>
-        /// <param name="resemble"></param>
         /// <param name="sightengine"></param>
+        /// <param name="resemble"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public VideodeepfakeDetectionAsyncModel(
-            global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? resemble,
-            global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? sightengine)
+            global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? sightengine,
+            global::EdenAI.VideodeepfakeDetectionAsyncDeepfakeDetectionAsyncDataClass? resemble)
         {
-            this.Resemble = resemble;
             this.Sightengine = sightengine;
+            this.Resemble = resemble;
         }
 
         /// <summary>
