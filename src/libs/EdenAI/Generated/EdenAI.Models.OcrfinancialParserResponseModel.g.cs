41a69c12c28487af819bd9b8561aa8a19da0ff04
Konstantin S. <havendv@gmail.com>
2026-09-13 02:45:07 +0400

    feat: Updated OpenAPI spec (#475)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EdenAI/Generated/EdenAI.Models.OcrfinancialParserResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.OcrfinancialParserResponseModel.g.cs
@@ -11,38 +11,14 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Google { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("tabscanner")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Tabscanner { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("eagledoc")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Eagledoc { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("affinda")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Affinda { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("base64")]
         public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Base64 { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Amazon { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("extracta")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Extracta { get; set; }
 
         /// <summary>
         ///
@@ -53,20 +29,14 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("klippa")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Klippa { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("eagledoc")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Eagledoc { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("mindee")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Mindee { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Openai { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("tabscanner")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Tabscanner { get; set; }
 
         /// <summary>
         ///
@@ -77,14 +47,44 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("google")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Google { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("mindee")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Mindee { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("affinda")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Affinda { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Openai { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("klippa")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Klippa { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
         public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Microsoft { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("extracta")]
-        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Extracta { get; set; }
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.OcrfinancialParserFinancialParserDataClass? Amazon { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -95,50 +95,50 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="OcrfinancialParserResponseModel" /> class.
         /// </summary>
-        /// <param name="google"></param>
-        /// <param name="tabscanner"></param>
-        /// <param name="eagledoc"></param>
-        /// <param name="affinda"></param>
         /// <param name="base64"></param>
-        /// <param name="amazon"></param>
-        /// <param name="veryfi"></param>
-        /// <param name="klippa"></param>
-        /// <param name="mindee"></param>
-        /// <param name="openai"></param>
-        /// <param name="dataleon"></param>
-        /// <param name="microsoft"></param>
         /// <param name="extracta"></param>
+        /// <param name="veryfi"></param>
+        /// <param name="eagledoc"></param>
+        /// <param name="tabscanner"></param>
+        /// <param name="dataleon"></param>
+        /// <param name="google"></param>
+        /// <param name="mindee"></param>
+        /// <param name="affinda"></param>
+        /// <param name="openai"></param>
+        /// <param name="klippa"></param>
+        /// <param name="microsoft"></param>
+        /// <param name="amazon"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public OcrfinancialParserResponseModel(
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? google,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? tabscanner,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? eagledoc,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? affinda,
             global::EdenAI.OcrfinancialParserFinancialParserDataClass? base64,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? amazon,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? extracta,
             global::EdenAI.OcrfinancialParserFinancialParserDataClass? veryfi,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? klippa,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? mindee,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? openai,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? eagledoc,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? tabscanner,
             global::EdenAI.OcrfinancialParserFinancialParserDataClass? dataleon,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? google,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? mindee,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? affinda,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? openai,
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? klippa,
             global::EdenAI.OcrfinancialParserFinancialParserDataClass? microsoft,
-            global::EdenAI.OcrfinancialParserFinancialParserDataClass? extracta)
+            global::EdenAI.OcrfinancialParserFinancialParserDataClass? amazon)
         {
-            this.Google = google;
-            this.Tabscanner = tabscanner;
-            this.Eagledoc = eagledoc;
-            this.Affinda = affinda;
             this.Base64 = base64;
-            this.Amazon = amazon;
-            this.Veryfi = veryfi;
-            this.Klippa = klippa;
-            this.Mindee = mindee;
-            this.Openai = openai;
-            this.Dataleon = dataleon;
-            this.Microsoft = microsoft;
             this.Extracta = extracta;
+            this.Veryfi = veryfi;
+            this.Eagledoc = eagledoc;
+            this.Tabscanner = tabscanner;
+            this.Dataleon = dataleon;
+            this.Google = google;
+            this.Mindee = mindee;
+            this.Affinda = affinda;
+            this.Openai = openai;
+            this.Klippa = klippa;
+            this.Microsoft = microsoft;
+            this.Amazon = amazon;
         }
 
         /// <summary>
--- src/libs/EdenAI/Generated/EdenAI.Models.TextkeywordExtractionResponseModel.g.cs
+++ src/libs/EdenAI/Generated/EdenAI.Models.TextkeywordExtractionResponseModel.g.cs
@@ -11,32 +11,8 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("corticalio")]
-        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Corticalio { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
-        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Amazon { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("oneai")]
         public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Oneai { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
-        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Emvista { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
-        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Openai { get; set; }
 
         /// <summary>
         ///
@@ -53,8 +29,32 @@ namespace EdenAI
         /// <summary>
         ///
         /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("emvista")]
+        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Emvista { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("openai")]
+        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Openai { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("microsoft")]
         public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Microsoft { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("corticalio")]
+        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Corticalio { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("amazon")]
+        public global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? Amazon { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -65,35 +65,35 @@ namespace EdenAI
         /// <summary>
         /// Initializes a new instance of the <see cref="TextkeywordExtractionResponseModel" /> class.
         /// </summary>
-        /// <param name="corticalio"></param>
-        /// <param name="amazon"></param>
         /// <param name="oneai"></param>
-        /// <param name="emvista"></param>
-        /// <param name="openai"></param>
         /// <param name="tenstorrent"></param>
         /// <param name="xai"></param>
+        /// <param name="emvista"></param>
+        /// <param name="openai"></param>
         /// <param name="microsoft"></param>
+        /// <param name="corticalio"></param>
+        /// <param name="amazon"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public TextkeywordExtractionResponseModel(
-            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? corticalio,
-            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? amazon,
             global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? oneai,
-            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? emvista,
-            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? openai,
             global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? tenstorrent,
             global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? xai,
-            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? microsoft)
+            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? emvista,
+            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? openai,
+            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? microsoft,
+            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? corticalio,
+            global::EdenAI.TextkeywordExtractionKeywordExtractionDataClass? amazon)
         {
-            this.Corticalio = corticalio;
-            this.Amazon = amazon;
             this.Oneai = oneai;
-            this.Emvista = emvista;
-            this.Openai = openai;
             this.Tenstorrent = tenstorrent;
             this.Xai = xai;
+            this.Emvista = emvista;
+            this.Openai = openai;
             this.Microsoft = microsoft;
+            this.Corticalio = corticalio;
+            this.Amazon = amazon;
         }
 
         /// <summary>
